0385260188611bb546e398d0c190dba80bf12e9f
NestorJauregui <ni.jauregui10@gmail.com>
2021-02-09 15:41:32 -0500

    Todas las partes logged-out de la página funcionan... falta incluir contenido en cada una.

--- LD50/website/views.py
+++ LD50/website/views.py
@@ -5,3 +5,31 @@ views = Blueprint('views', __name__)
 @views.route('/')
 def home():
     return render_template("home.html")
+
+@views.route('/nosotros')
+def nosotros():
+    return render_template("nosotros.html")
+
+@views.route('/nuestro-trabajo')
+def nuestro_trabajo():
+    return render_template("nuestro_trabajo.html")
+
+@views.route('/nuestro-equipo')
+def nuestro_equipo():
+    return render_template("nuestro_equipo.html")
+
+@views.route('/letal-shit')
+def letal_shit():
+    return render_template("letal_shit.html")
+
+@views.route('/beats')
+def beats():
+    return render_template("beats.html")
+
+@views.route('/blog')
+def blog():
+    return render_template("blog.html")
+
+@views.route('/contacto')
+def contacto():
+    return render_template("contacto.html")
